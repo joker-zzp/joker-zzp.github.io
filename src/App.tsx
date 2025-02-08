03540aac22a23b6062ae8959d5ae95a7960619f0
joker-zzp <joker_zzp@qq.com>
2025-02-08 14:11:48 +0800

    init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,13 @@
-import * as React from 'react';
-import IconButton from '@mui/material/IconButton';
-import Box from '@mui/material/Box';
-import { useTheme, ThemeProvider } from '@mui/material/styles';
-import Brightness4Icon from '@mui/icons-material/Brightness4';
-import Brightness7Icon from '@mui/icons-material/Brightness7';
-import { purpleTheme } from './theme';
-import { Button } from '@mui/material';
 
-const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
+function App() {
 
-function MyApp() {
-  const theme = useTheme();
-  const colorMode = React.useContext(ColorModeContext);
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        // width: '100%',
-        height: '100%',
-        alignItems: 'center',
-        justifyContent: 'center',
-        bgcolor: 'background.default',
-        color: 'text.primary',
-        // p: 3,
-      }}
-    >
-      {theme.palette.mode} mode
-      <Button variant='contained'>mode</Button>
-      <Button variant='contained' color='secondary' >mode</Button>
-      <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode} color='secondary'>
-        {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
-      </IconButton>
-    </Box>
-  );
-}
-
-export function ToggleColorMode() {
-  const [mode, setMode] = React.useState<'light' | 'dark'>('light');
-  const colorMode = React.useMemo(
-    () => ({
-      toggleColorMode: () => {
-        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
-      },
-    }),
-    [],
-  );
-
-  const theme = React.useMemo(
-    () => purpleTheme(mode),
-    [mode],
+    <>
+      <div>
+        <p>hello</p>
+      </div>
+    </>
   )
-
-  return (
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        <MyApp />
-      </ThemeProvider>
-    </ColorModeContext.Provider>
-  );
 }
 
-export default ToggleColorMode;
+export default App
